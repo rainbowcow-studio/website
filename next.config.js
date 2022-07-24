175227f74d49f25f435a2ed6fe23f9c8580f4470
Jeremy Kahn <jeremyckahn@gmail.com>
2022-07-24 12:01:54 -0500

    chore: set up deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+// GitHub Pages deployment code comes from:
+// https://gregrickaby.blog/2020/03/21/next-js-github-pages
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/website/' : ''
 }
 
 module.exports = nextConfig
