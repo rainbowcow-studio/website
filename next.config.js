b3e670eeea82751048853ae730de590dfc15fda6
Jeremy Kahn <jeremyckahn@gmail.com>
2022-07-25 22:03:53 -0500

    feat: stand up some basic content

--- next.config.js
+++ next.config.js
@@ -3,18 +3,20 @@
 // GitHub Pages deployment code comes from:
 // https://gregrickaby.blog/2020/03/21/next-js-github-pages
 
+const withMDX = require('@next/mdx')()
+
 const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig = {
+const nextConfig = withMDX({
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/website/' : '',
+  assetPrefix: isProd ? '/website/' : '.',
 
   experimental: {
     images: {
       unoptimized: true,
     },
   },
-}
+})
 
 module.exports = nextConfig
