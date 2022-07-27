ac01717743c2fd19788a2793496dd0c43c9b91ee
Jeremy Kahn <jeremyckahn@gmail.com>
2022-07-27 09:34:39 -0500

    feat: add some basic content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactMarkdown from 'react-markdown'
+import logo from './img/rainbow-cow-logo.gif'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" />
       </header>
+      <main>
+        <ReactMarkdown>{`Hello! We are Rainbow Cow, AKA [Jeremy Kahn](https://github.com/jeremyckahn/) and [Luke Stebner](https://github.com/lstebner/). We're a pair of developers who love making fun games for people.`}</ReactMarkdown>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
