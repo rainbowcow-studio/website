511ff75257524e0943a036816a0892118129a7ea
jeremyckahn <jeremyckahn@users.noreply.github.com>
2022-07-30 16:27:06 +0000

    deploy: e190010379c082a6f230430db842cad82eebcbb3

--- _next/static/LjDW9Wi2i2DeLc7k21rrM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1472d148f27f0437.css","static/chunks/pages/index-c7635781dc230d56.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
